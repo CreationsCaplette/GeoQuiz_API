66844a34c27f6809460eead67cc0193e7a1eb4f6
CreationsCaplette <david.caplette@gmail.com>
2026-06-20 16:26:52 +0200

    Added basic conversion of data

---
+++ GeoQuiz_API/Data/RestCountriesResponseConverter.cs
@@ -0,0 +1,27 @@
+﻿using GeoQuiz_API.Data.GeoQuiz;
+using GeoQuiz_API.Data.RestCountries;
+
+namespace GeoQuiz_API.Data;
+
+public static class RestCountriesResponseConverter
+{
+    public static List<GeoQuizCountry> ConvertToGeoQuizCountries(this RestCountriesResponse response)
+    {
+        var countries = new List<GeoQuizCountry>();
+
+        foreach (var obj in response.data.objects)
+        {
+            if (obj.capitals.Count == 0 || string.IsNullOrWhiteSpace(obj.flag.url_svg))
+                continue;
+
+            countries.Add(new GeoQuizCountry
+            (
+                obj.names.common,
+                obj.capitals[0].name,
+                obj.flag.url_svg
+            ));
+        }
+
+        return countries;
+    }
+}
--- GeoQuiz_API/Program.cs
+++ GeoQuiz_API/Program.cs
@@ -1,3 +1,5 @@
+using GeoQuiz_API.Data;
+using GeoQuiz_API.Data.GeoQuiz;
 using GeoQuiz_API.Data.RestCountries;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,13 +21,22 @@ app.UseHttpsRedirection();
 app.MapGet("/countries", async (IConfiguration config) =>
 {
     var apiKey = config.GetValue<string>("RestCountriesApiKey");
-    var limit = 100;
-    var offset = 0;
 
     var client = new HttpClient();
     client.DefaultRequestHeaders.Add("Authorization", apiKey);
-    var data = await client.GetFromJsonAsync<RestCountriesResponse>($"https://api.restcountries.com/countries/v5?limit={limit}&offset={offset}&response_fields=names.common,capitals.name,flag.url_svg");
-    return data;
+
+    var limit = 100;
+    var offset = 0;
+    var uri = $"https://api.restcountries.com/countries/v5?limit={limit}&offset={offset}&response_fields=names.common,capitals.name,flag.url_svg";
+    var data = await client.GetFromJsonAsync<RestCountriesResponse>(uri);
+
+    if (data is null)
+    {
+        return [];
+    }
+
+    var countries = data.ConvertToGeoQuizCountries();
+    return countries;
 })
 .WithName("CountriesData");
 
