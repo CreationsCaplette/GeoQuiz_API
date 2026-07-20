519f895bce85a195ece632b0ddc078634f60f697
CreationsCaplette <david.caplette@gmail.com>
2026-07-20 20:24:25 +0200

    Added GameCapitalsReverseRepository and simplify everything by using a base class

---
+++ GeoQuiz_API/Repositories/BaseGameCapitalsRepository.cs
@@ -0,0 +1,70 @@
+﻿using GeoQuiz_API.Helper;
+using GeoQuiz_API.Models.GeoQuiz;
+using Microsoft.Extensions.Options;
+using System.Runtime.InteropServices;
+
+namespace GeoQuiz_API.Repositories;
+
+public abstract class BaseGameCapitalsRepository(
+    IOptions<GeoQuizGameOptions> options,
+    IGeoQuizRepository geoQuizRepository,
+    IRandomProvider randomProvider
+    )
+{
+    protected readonly GeoQuizGameOptions GameOptions = options.Value;
+    protected readonly IGeoQuizRepository GeoQuizRepository = geoQuizRepository;
+    protected readonly IRandomProvider RandomProvider = randomProvider;
+
+    protected async Task<List<GeoQuizQuestion>> GetGameQuestions(
+        Func<GeoQuizCountry, string> questionSelector,
+        Func<GeoQuizCountry, string> answerSelector)
+    {
+        var countries = await GeoQuizRepository.GetAllGeoQuizCountries();
+        var questions = new List<GeoQuizQuestion>();
+
+        var shuffledCountries = countries.ToList();
+        RandomProvider.Shuffle(CollectionsMarshal.AsSpan(shuffledCountries));
+
+        for (int i = 0; i < GameOptions.NumberOfQuestions; i++)
+        {
+            var correctCountry = shuffledCountries[i % shuffledCountries.Count];
+            var choices = GenerateChoices(correctCountry, countries, answerSelector);
+
+            questions.Add(new GeoQuizQuestion(
+                questionSelector(correctCountry),
+                choices,
+                answerSelector(correctCountry)
+            ));
+        }
+
+        return questions;
+    }
+
+    private List<string> GenerateChoices(
+        GeoQuizCountry correctCountry,
+        List<GeoQuizCountry> allCountries,
+        Func<GeoQuizCountry, string> answerSelector)
+    {
+        var choices = new List<string> { answerSelector(correctCountry) };
+
+        while (choices.Count < GameOptions.NumberOfChoices)
+        {
+            var randomChoice = answerSelector(allCountries[RandomProvider.Next(allCountries.Count)]);
+            choices.Add(randomChoice);
+        }
+
+        var uniqueChoices = choices.Distinct().ToList();
+
+        while (uniqueChoices.Count < GameOptions.NumberOfChoices)
+        {
+            var randomChoice = answerSelector(allCountries[RandomProvider.Next(allCountries.Count)]);
+            if (!uniqueChoices.Contains(randomChoice))
+            {
+                uniqueChoices.Add(randomChoice);
+            }
+        }
+
+        RandomProvider.Shuffle(CollectionsMarshal.AsSpan(uniqueChoices));
+        return uniqueChoices;
+    }
+}
--- GeoQuiz_API/Repositories/GameCapitalsRepository.cs
+++ GeoQuiz_API/Repositories/GameCapitalsRepository.cs
@@ -1,7 +1,6 @@
 ﻿using GeoQuiz_API.Helper;
 using GeoQuiz_API.Models.GeoQuiz;
 using Microsoft.Extensions.Options;
-using System.Runtime.InteropServices;
 
 namespace GeoQuiz_API.Repositories;
 
@@ -9,55 +8,11 @@ public class GameCapitalsRepository(
     IOptions<GeoQuizGameOptions> options,
     IGeoQuizRepository geoQuizRepository,
     IRandomProvider randomProvider
-    ) : IGameCapitalsRepository
+) : BaseGameCapitalsRepository(options, geoQuizRepository, randomProvider), IGameCapitalsRepository
 {
-    private readonly GeoQuizGameOptions GameOptions = options.Value;
-
-    public async Task<List<GeoQuizQuestion>> GetCapitalsGame()
-    {
-        var countries = await geoQuizRepository.GetAllGeoQuizCountries();
-        var questions = new List<GeoQuizQuestion>();
-
-        var shuffledCountries = countries.ToList();
-        randomProvider.Shuffle(CollectionsMarshal.AsSpan(shuffledCountries));
-
-        for (int i = 0; i < GameOptions.NumberOfQuestions; i++)
-        {
-            var correctCountry = shuffledCountries[i % shuffledCountries.Count];
-            var choices = GenerateChoices(correctCountry, countries);
-
-            questions.Add(new GeoQuizQuestion(
-                correctCountry.CountryName,
-                choices,
-                correctCountry.CapitalName
-            ));
-        }
-
-        return questions;
-    }
-
-    private List<string> GenerateChoices(GeoQuizCountry correctCountry, List<GeoQuizCountry> allCountries)
-    {
-        var choices = new List<string> { correctCountry.CapitalName };
-
-        while (choices.Count < GameOptions.NumberOfChoices)
-        {
-            var randomCapital = allCountries[randomProvider.Next(allCountries.Count)].CapitalName;
-            choices.Add(randomCapital);
-        }
-
-        var uniqueChoices = choices.Distinct().ToList();
-
-        while (uniqueChoices.Count < GameOptions.NumberOfChoices)
-        {
-            var randomCapital = allCountries[randomProvider.Next(allCountries.Count)].CapitalName;
-            if (!uniqueChoices.Contains(randomCapital))
-            {
-                uniqueChoices.Add(randomCapital);
-            }
-        }
-
-        randomProvider.Shuffle(CollectionsMarshal.AsSpan(uniqueChoices));
-        return uniqueChoices;
-    }
+    public Task<List<GeoQuizQuestion>> GetCapitalsGame() =>
+        GetGameQuestions(
+            country => country.CountryName,
+            country => country.CapitalName
+        );
 }
